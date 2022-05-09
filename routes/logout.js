f6f585dfe3d4f280dc74bb0ca76720a3f4f097ac
firewallfail <firewall.fail@hotmail.com>
2022-05-09 18:52:12 -0400

    Add ability to logout and clear cookies

---
+++ routes/logout.js
@@ -0,0 +1,23 @@
+/*
+ * All routes for home are defined here
+ *   these routes are mounted onto /
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    res.clearCookie('user_id');
+    res.clearCookie('user_id.sig');
+    return res.redirect("/order");
+  });
+
+  router.post("/", (req, res) => {
+    res.clearCookie('user_id');
+    res.clearCookie('user_id.sig');
+    return res.redirect("/order");
+  });
+  return router;
+};
