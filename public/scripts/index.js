39661dbf15ab22936346f5f183552b9e83e1aeff
Matthieu Pauze <matt.pauze@gmail.com>
2022-05-11 17:52:23 -0400

    Feature: Implement cart using cookies

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -11,7 +11,8 @@ $(document).ready(() => {
         $article.append($leftDiv);
         $article.append(`<span class="food-description">${item.description}</span>`);
         $rightDiv.append(`<span class="food-cost">${toDollar(item.price)}</span>`);
-        $(`<button class="btn btn-success" data-food-id=${item.id}>Add</button>`).on('click', addToCart).on('click', updateCart).appendTo($rightDiv);
+        $(`<button class="btn btn-success" data-food-id=${item.id}>Add</button>`).on('click', addToCart).on('click',
+          updateCart).appendTo($rightDiv);
         $article.append($rightDiv);
         $menu.append($article);
       }
@@ -19,20 +20,56 @@ $(document).ready(() => {
     .fail(error => {
       console.log(error);
     });
+  updateCart();
 });
 
 const toDollar = (centsValue) => {
   return "$" + (centsValue / 100).toFixed(2);
 };
 
-const addToCart = function() {
-  const { foodId } = this.dataset;
-  const cart = JSON.parse(Cookies.get("cart") || "{}");
-  cart[foodId] = ++cart[foodId] || 1;
-  console.log(cart);
-  Cookies.set("cart", JSON.stringify(cart));
+const getCart = () => {
+  return JSON.parse(Cookies.get("cart") || "{}");
 };
 
-const updateCart = function() {
+const setCart = (object) => {
+  Cookies.set("cart", JSON.stringify(object));
+};
 
+const addToCart = function () {
+  const { foodId } = this.dataset;
+  const cart = getCart();
+  cart[foodId] = ++cart[foodId] || 1;
+  console.log(cart);
+  setCart(cart);
+};
+
+const updateCart = function () {
+  const $cartMenu = $("#cart-items");
+  const cart = getCart();
+  $cartMenu.empty();
+
+  if ($.isEmptyObject(cart)) {
+    return $cartMenu.append(`<li><span class="dropdown-item-text">Your cart is currently empty!</span></li>`);
+  }
+
+  for (const id in cart) {
+    $.get(`/food/${id}`)
+      .then(item => {
+        $cartMenu.append(`
+        <li class="item" data-id="${item.id}">
+          <div class="left-group">
+            <img src="${item.picture}" alt="" />
+            <div class="item-info">
+              <span>${item.item}</span>
+              <span>Price: ${toDollar(item.price)}</span>
+            </div>
+          </div>
+          <button class="btn btn-danger fa fa-close"></button>
+        </li>
+      `);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 };
