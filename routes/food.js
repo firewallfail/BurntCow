1082a18eb89d77e2016fb7a03731f79dfa4c1c3b
Matthieu Pauze <matt.pauze@gmail.com>
2022-05-11 14:46:39 -0400

    Feature: Implement search for food

--- routes/food.js
+++ routes/food.js
@@ -9,20 +9,51 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`
+    const limit = req.query.limit || 10;
+    const offset = req.query.offset || 0;
+    const search = req.query.search;
+
+    let queryString = `
     SELECT *
     FROM menu_items
-    LIMIT 10;
-    `)
+    `;
+    const queryParams = [limit, offset];
+
+    if (search) {
+      queryParams.push(`%${search}%`);
+      queryString += `WHERE item ILIKE $${queryParams.length} OR description ILIKE $${queryParams.length}`;
+    }
+
+    queryString += `
+    LIMIT $1
+    OFFSET $2;
+    `;
+
+    console.log(queryString, queryParams);
+    db.query(queryString, queryParams)
       .then(data => {
         res.json(data.rows);
       })
       .catch(error => {
         console.log(error);
+        res.status(500).json("An error occured");
       });
   });
 
-  router.get("/");
+  router.get("/:id", (req, res) => {
+    db.query(`
+    SELECT *
+    FROM menu_items
+    WHERE id = $1;
+    `, [req.params.id])
+      .then(data => {
+        res.json(data.rows[0] || {});
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json("An error occured");
+      });
+  });
 
   return router;
 };
