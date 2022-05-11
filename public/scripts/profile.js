734547c418be27161eed38f3fe4908c31c10096f
firewallfail <firewall.fail@hotmail.com>
2022-05-11 15:37:35 -0400

    New password fields can be blank now but both must match

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -3,4 +3,19 @@ $(document).ready(() => {
     mask: "(999) 999-9999",
     autoUnmask: true
   });
+
+  const passwordInput = document.querySelector("#passwordInput");
+  const confirmPasswordInput = document.querySelector("#confirmPasswordInput");
+
+  const validatePassword = () => {
+    if (passwordInput.value !== confirmPasswordInput.value) {
+      confirmPasswordInput.setCustomValidity('Passwords must match');
+    } else {
+      confirmPasswordInput.setCustomValidity('');
+    }
+  };
+
+  passwordInput.addEventListener('change', validatePassword);
+  confirmPasswordInput.addEventListener('change', validatePassword);
+
 });
