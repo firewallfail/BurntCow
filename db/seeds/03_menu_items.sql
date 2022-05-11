e4f392dd825b2b37d85553fe0b1c6cf7bc102d12
firewallfail <firewall.fail@hotmail.com>
2022-05-11 09:17:29 -0400

    Add real menu items to db

--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -48,3 +48,25 @@ INSERT INTO menu_items (item, picture, price, description) VALUES ('Sole - Fille
 INSERT INTO menu_items (item, picture, price, description) VALUES ('Propel Sport Drink', 'http://dummyimage.com/249x100.png/dddddd/000000', 1189, 'nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat eros viverra eget congue eget semper rutrum nulla nunc purus phasellus in felis donec semper');
 INSERT INTO menu_items (item, picture, price, description) VALUES ('Capicola - Hot', 'http://dummyimage.com/207x100.png/cc0000/ffffff', 1981, 'luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus etiam');
 INSERT INTO menu_items (item, picture, price, description) VALUES ('Vinegar - White Wine', 'http://dummyimage.com/208x100.png/dddddd/000000', 669, 'dui vel sem sed sagittis nam congue risus semper porta volutpat quam pede lobortis ligula sit amet eleifend pede libero quis orci nullam molestie nibh in lectus pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate luctus cum sociis natoque penatibus et magnis');
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Top Bun', '1', 1074, 'Two beef patties, fancy sauce, cheese, pickles, chopped lettuce, diced onion.')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('The Richmond', '1', 1074, 'Double cheese burger with bacon & caramelized onions.')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Varsity Blues', '1', 1099, 'Two beef patties, bacon & blue cheese.')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Say Cheese', '1', 1149, 'Double cheeseburger.')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('The Portobello', '1', 874, 'A Portobello stuffed with herbed cheese, rolled in panko crumbs, & fried.')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Chicken Burger', '1', 999, 'Fried chicken on a sesame bun.')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Hamburger', '1', 699, 'Old fashioned hamburger.')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Cheeseburger', '1', 749, 'Old fashioned hamburger...with cheese.')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Baconburger', '1', 749, 'Old fashioned hamburger...with bacon.')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Soy Burger', '', 799, 'Hamburger hold the ham.')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('', '', , '')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('', '', , '')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Small Drink', '1', 199, 'Small fountain pop')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Medium Drink', '1', 249, 'Medium fountain pop')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Large Drink', '1', 299, 'Large fountain pop')
+
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Small Fries', '1', 299, 'Small order of fries')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Regular Fries', '1', 399, 'Regular order of fries')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Large Fries', '1', 499, 'Large order of fries')
+
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Chocolate Shake', '1', 449, 'Delicious chocolate milk shake')
+INSERT INTO menu_items (item, picture, price, description) VALUES ('Strawberry Shake', '1', 449, 'Delicious strawberry milk shake')
