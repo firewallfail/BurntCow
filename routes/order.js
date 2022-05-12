72443905116a351bf0e00fb3407c476485f017dd
firewallfail <firewall.fail@hotmail.com>
2022-05-12 17:52:06 -0400

    Create array of query values and create queryParams

--- routes/order.js
+++ routes/order.js
@@ -24,18 +24,66 @@ module.exports = (db) => {
 
     const incomingOrder = { "51": 1, "52": 1, "53": 1, "54": 1, "55": 2 };
 
-    console.log(incomingOrder);
+    const userId = 1;
+    const timeOrdered = new Date(Date.now());
 
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
+    // const userId = req.session.userId;
+    const values = [userId, timeOrdered];
+
+    db.query(`
+    INSERT INTO orders (user_id, time_ordered)
+    VALUES
+    ($1, $2)
+    RETURNING id;
+    `, values)
+      .then((result) => {
+        const orderId = result.rows[0].id;
+
+        const values = [];
+
+        for (const key in incomingOrder) {
+          values.push(orderId);
+          values.push(Number(key));
+          values.push(incomingOrder[key]);
+        }
+
+        let queryParams = `INSERT INTO ordered_items (order_id, item_id, quantity) VALUES `;
+
+        for (let i = 1; i < values.length; i += 3) {
+          queryParams += (`($${i}, $${i + 1}, $${i + 2})`);
+          if (i + 2 < values.length) {
+            queryParams += `, `
+          }
+        }
+
+
+
+        console.log(queryParams);
+
+        return res.status(200).json(queryParams);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+      .catch((err) => {
+        console.log(err.message)
+      })
+
+    // client.messages
+    //   .create({
+    //     body: `POSTING`,
+    //     from: twilioNumber,
+    //     to: twilioReceivingNumber //TODO: Make this the result of a database query for the user's phone number
+    //   })
+    //   .then(message => console.log(message.sid));
+
+    // db.query(`SELECT * FROM users;`)
+    //   .then(data => {
+    //     const users = data.rows;
+    //     res.json({ users });
+    //   })
+    //   .catch(err => {
+    //     res
+    //       .status(500)
+    //       .json({ error: err.message });
+    //   });
   });
   return router;
 };
